d21427e8ebc697395643ef218f9529630f365a60
GokuPlay609 <120821054+GokuPlay609@users.noreply.github.com>
2024-07-10 23:08:09 +0530

    Update ban.py

--- commands/ban.py
+++ commands/ban.py
@@ -1,22 +1,21 @@
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import CallbackContext
 
-async def ban_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def ban_command(update: Update, context: CallbackContext):
     if update.message.chat.type not in ['group', 'supergroup']:
-        await update.message.reply_text("This command can only be used in groups.")
+        update.message.reply_text("This command can only be used in groups.")
         return
     
     if not update.message.reply_to_message:
-        await update.message.reply_text("You must reply to a message to ban the user.")
+        update.message.reply_text("You must reply to a message to ban the user.")
         return
     
     # Check if the user is an admin
-    chat_member = await context.bot.get_chat_member(update.message.chat.id, update.message.from_user.id)
+    chat_member = context.bot.get_chat_member(update.message.chat.id, update.message.from_user.id)
     if chat_member.status not in ['administrator', 'creator']:
-        await update.message.reply_text("You don't have the privileges to execute this command.")
+        update.message.reply_text("You don't have the privileges to execute this command.")
         return
 
     user_id = update.message.reply_to_message.from_user.id
-    await context.bot.ban_chat_member(update.message.chat.id, user_id)
-    await update.message.reply_text("User has been banned.")
-  
+    context.bot.kick_chat_member(update.message.chat.id, user_id)
+    update.message.reply_text("User has been banned.")
