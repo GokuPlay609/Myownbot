84940a751423b159757c175bc19d6f4a9e170851
GokuPlay609 <120821054+GokuPlay609@users.noreply.github.com>
2024-07-11 07:17:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
     from commands.mute import mute_command
     from commands.unmute import unmute_command
     from commands.promote import promote_command
-    from commands.myinfo import myinfo_command
+    from commands.myinfo import myinfo_command, count_messages
 
     dispatcher.add_handler(CommandHandler("start", start_command))
     dispatcher.add_handler(CommandHandler("help", help_command))
@@ -56,8 +56,7 @@ def main():
     dispatcher.add_handler(CommandHandler("unmute", unmute_command))
     dispatcher.add_handler(CommandHandler("promote", promote_command, pass_args=True))
     dispatcher.add_handler(CommandHandler("myinfo", myinfo_command))
-
-    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), myinfo_command))
+    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), count_messages))
 
     # Log all errors
     dispatcher.add_error_handler(error_handler)
