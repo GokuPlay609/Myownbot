d042d609ca10f0c718be1e04e64c4fcea27841c5
GokuPlay609 <120821054+GokuPlay609@users.noreply.github.com>
2024-07-11 07:17:36 +0530

    Update myinfo.py

--- commands/myinfo.py
+++ commands/myinfo.py
@@ -1,18 +1,16 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 
-# A simple dictionary to keep track of the number of messages sent by users
-user_message_count = {}
-
 def myinfo_command(update: Update, context: CallbackContext):
     user = update.message.from_user
-    user_id = user.id
-    user_info = f"User ID: {user_id}\nUsername: {user.username}\nFirst Name: {user.first_name}\nLast Name: {user.last_name}"
-    
-    # Update the message count for the user
-    user_message_count[user_id] = user_message_count.get(user_id, 0) + 1
-    message_count = user_message_count[user_id]
-    
-    user_info += f"\nMessages Sent: {message_count}"
-    
+    user_info = (
+        f"User ID: {user.id}\n"
+        f"Username: @{user.username}\n"
+        f"First name: {user.first_name}\n"
+        f"Last name: {user.last_name}\n"
+        f"Messages sent: {context.user_data.get('message_count', 0)}"
+    )
     update.message.reply_text(user_info)
+
+def count_messages(update: Update, context: CallbackContext):
+    context.user_data['message_count'] = context.user_data.get('message_count', 0) + 1
